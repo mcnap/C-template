e3f6a1190f5c5dc3a40a8205e1d270227b216fa8
mcnap <152072535+mcnap@users.noreply.github.com>
2024-12-05 21:43:32 +0100

    fatal_usage

--- src/main.c
+++ src/main.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void fatal_usage(char const *const prog_name)
+{
+    fprintf(stderr, "USAGE: %s \n", prog_name);
+    exit(EXIT_FAILURE);
+}
+
 int main(int const argc, char const *const *const argv)
 {
     char const text[20] = "Hello, world!\n";
